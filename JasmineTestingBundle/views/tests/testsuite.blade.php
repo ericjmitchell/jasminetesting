b4cba944bf42303aceb93d4364d4e9bafaaa04f2
Eric Mitchell <ericmitchell42@gmail.com>
2013-04-02 17:38:40 -0600

    Fixed the strings in createtestroutes.php.

--- JasmineTestingBundle/views/tests/testsuite.blade.php
+++
@@ -1,9 +0,0 @@
-@layout('jasminetesting::tests.testsuitetemplate')
-
-@section('title')
-@endsection
-
-@section('assets')
-<?php echo Asset::container('foo')->styles(); echo Asset::container('foo')->scripts();
-      echo Asset::styles(); echo Asset::scripts();?>
-@endsection
--- tasks/createtestroutes.php
+++ tasks/createtestroutes.php
@@ -3,11 +3,16 @@
 use Laravel\CLI\Command as Command;
 use Laravel\Bundle as Bundle;
 
+<<<<<<< HEAD
 class JasmineTestingBundle_CreateTestRoutes_Task
+=======
+class JasmineTesting_CreateTestRoutes_Task
+>>>>>>> dev1
 {
     public function run($arguments)
     {
         $pgconfig = 0;
+<<<<<<< HEAD
         require Bundle::path('jasminetestingbundle').'pgtestconfig.php';
         $path = Bundle::path('jasminetestingbundle').'routes.php';
         
@@ -65,6 +70,47 @@ Route::get(\'/'.$pgconfig["testRoute"].'\', array(\'as\' => \'testRoute\', funct
         
         File::put($path, $data);
         echo 'routes.php updated!';
+=======
+        require Bundle::path('jasminetesting').'config\config.php';
+        $path = Bundle::path('jasminetesting').'routes.php';
+        
+        $data = "<?php";
+        $data .= "\nRoute::get(\'/{$pgconfig["testRoute"]}\', array(\'as\' => \'testRoute\', function() {\n\n\t";
+        foreach ($pgconfig["commonAssets"] as $item)
+        {
+            $data .= "Asset::add(\'$item\', \'$item\');\n\t";
+        }
+            
+        $data .= "return View::make(\'jasminetesting::tests.testhome\')->with(\'runners\', array(";
+        foreach ($pgconfig["testRunners"] as $name => $value)
+        {
+            $data .= "\'$name\',"; 
+        }
+        $data .= "));\n}));\n\n";
+        
+        foreach ($pgconfig["testRunners"] as $key => $value)
+        {
+            $data .= "Route::get(\'/{$pgconfig["testRoute"]}/$key\', function()\n{\n\t";
+            $data .= "Asset::container(\'foo\')->bundle(\'jasminetesting');";
+            $data .= "Asset::container(\'foo\')->add(\'jasmine.css\', \'css/jasmine.css\');\n\t";
+            $data .= "Asset::container(\'foo\')->add(\'jasmine.js\', \'js/jasmine.js\');\n\t";
+            $data .= "Asset::container(\'foo\')->add(\'jasmine-html.js\', \'js/jasmine-html.js\');\n\t";
+            foreach ($value["scripts"] as $item)
+            {
+                $data .= "Asset::add(\'$item\', \'$item\');\n\t";
+            }
+            foreach ($value["suites"] as $item)
+            {
+                $data .= "Asset::add(\'$item\', \'$item\');\n\t";
+            }
+            $data .= "Section::inject(\'title\', \'$key Tests\');\n\n\treturn View::make(\'jasminetesting::tests.testsuite\');\n});\n";
+        }
+        
+        $data .= "?>";
+        
+        File::put($path, $data);
+        echo "routes.php updated!";
+>>>>>>> dev1
     }
 }
 ?>
