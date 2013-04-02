7b56ccc57d6c6fd8bb6e78076fce48e1ef22baf9
Eric Mitchell <ericmitchell42@gmail.com>
2013-04-02 17:58:01 -0600

    Fixed the merge conflict.

--- tasks/createtestroutes.php
+++ tasks/createtestroutes.php
@@ -3,74 +3,11 @@
 use Laravel\CLI\Command as Command;
 use Laravel\Bundle as Bundle;
 
-<<<<<<< HEAD
-class JasmineTestingBundle_CreateTestRoutes_Task
-=======
 class JasmineTesting_CreateTestRoutes_Task
->>>>>>> dev1
 {
     public function run($arguments)
     {
         $pgconfig = 0;
-<<<<<<< HEAD
-        require Bundle::path('jasminetestingbundle').'pgtestconfig.php';
-        $path = Bundle::path('jasminetestingbundle').'routes.php';
-        
-        $data = '<?php';
-        $data .= '
-Route::get(\'/'.$pgconfig["testRoute"].'\', array(\'as\' => \'testRoute\', function() {
-    
-    ';
-        foreach ($pgconfig["commonAssets"] as $item)
-        {
-            $data .= 'Asset::add(\''.$item.'\', \''.$item.'\');
-    ';
-        }
-            
-        $data .= 'return View::make(\'jasminetestingbundle::tests.testhome\')->with(\'runners\', array(';
-        foreach ($pgconfig["testRunners"] as $name => $value)
-        {
-            $data .= '\''.$name.'\','; 
-        }
-        $data .= '));
-}));
-
-';
-        
-        foreach ($pgconfig["testRunners"] as $key => $value)
-        {
-            $data .= 'Route::get(\'/'.$pgconfig["testRoute"].'/'.$key.'\', function()
-{
-    ';
-            $data .= 'Asset::container(\'foo\')->bundle(\'jasminetestingbundle\');';
-            $data .= 'Asset::container(\'foo\')->add(\'jasmine.css\', \'css/jasmine.css\');
-    ';
-            $data .= 'Asset::container(\'foo\')->add(\'jasmine.js\', \'js/jasmine.js\');
-    ';
-            $data .= 'Asset::container(\'foo\')->add(\'jasmine-html.js\', \'js/jasmine-html.js\');
-    ';
-            foreach ($value["scripts"] as $item)
-            {
-                $data .= 'Asset::add(\'' . $item . '\', \'' . $item . '\');
-    ';
-            }
-            foreach ($value["suites"] as $item)
-            {
-                $data .= 'Asset::add(\'' . $item . '\', \'' . $item . '\');
-    ';
-            }
-            $data .= 'Section::inject(\'title\', \''.$key.' Tests\');
-
-    return View::make(\'jasminetestingbundle::tests.testsuite\');
-});
-';
-        }
-        
-        $data .= '?>';
-        
-        File::put($path, $data);
-        echo 'routes.php updated!';
-=======
         require Bundle::path('jasminetesting').'config\config.php';
         $path = Bundle::path('jasminetesting').'routes.php';
         
@@ -110,7 +47,6 @@ Route::get(\'/'.$pgconfig["testRoute"].'\', array(\'as\' => \'testRoute\', funct
         
         File::put($path, $data);
         echo "routes.php updated!";
->>>>>>> dev1
     }
 }
 ?>
