3abf0d83fcb1cea67e5f4e5dc489363f5b60f842
Eric Mitchell <ericmitchell42@gmail.com>
2013-04-02 18:42:59 -0600

    Fixed a string error.

--- tasks/createtestroutes.php
+++ tasks/createtestroutes.php
@@ -12,35 +12,35 @@ class JasmineTesting_CreateTestRoutes_Task
         $path = Bundle::path('jasminetesting').'routes.php';
         
         $data = "<?php";
-        $data .= "\nRoute::get(\'/{$pgconfig["testRoute"]}\', array(\'as\' => \'testRoute\', function() {\n\n\t";
+        $data .= "\nRoute::get('/{$pgconfig["testRoute"]}', array('as' => 'testRoute', function() {\n\n\t";
         foreach ($pgconfig["commonAssets"] as $item)
         {
-            $data .= "Asset::add(\'$item\', \'$item\');\n\t";
+            $data .= "Asset::add('$item', '$item');\n\t";
         }
             
-        $data .= "return View::make(\'jasminetesting::tests.testhome\')->with(\'runners\', array(";
+        $data .= "return View::make('jasminetesting::tests.testhome')->with('runners', array(";
         foreach ($pgconfig["testRunners"] as $name => $value)
         {
-            $data .= "\'$name\',"; 
+            $data .= "'$name',"; 
         }
         $data .= "));\n}));\n\n";
         
         foreach ($pgconfig["testRunners"] as $key => $value)
         {
-            $data .= "Route::get(\'/{$pgconfig["testRoute"]}/$key\', function()\n{\n\t";
-            $data .= "Asset::container(\'foo\')->bundle(\'jasminetesting');";
-            $data .= "Asset::container(\'foo\')->add(\'jasmine.css\', \'css/jasmine.css\');\n\t";
-            $data .= "Asset::container(\'foo\')->add(\'jasmine.js\', \'js/jasmine.js\');\n\t";
-            $data .= "Asset::container(\'foo\')->add(\'jasmine-html.js\', \'js/jasmine-html.js\');\n\t";
+            $data .= "Route::get('/{$pgconfig["testRoute"]}/$key', function()\n{\n\t";
+            $data .= "Asset::container('foo')->bundle('jasminetesting');";
+            $data .= "Asset::container('foo')->add('jasmine.css', 'css/jasmine.css');\n\t";
+            $data .= "Asset::container('foo')->add('jasmine.js', 'js/jasmine.js');\n\t";
+            $data .= "Asset::container('foo')->add('jasmine-html.js', 'js/jasmine-html.js');\n\t";
             foreach ($value["scripts"] as $item)
             {
-                $data .= "Asset::add(\'$item\', \'$item\');\n\t";
+                $data .= "Asset::add('$item', '$item');\n\t";
             }
             foreach ($value["suites"] as $item)
             {
-                $data .= "Asset::add(\'$item\', \'$item\');\n\t";
+                $data .= "Asset::add('$item', '$item');\n\t";
             }
-            $data .= "Section::inject(\'title\', \'$key Tests\');\n\n\treturn View::make(\'jasminetesting::tests.testsuite\');\n});\n";
+            $data .= "Section::inject('title', '$key Tests');\n\n\treturn View::make('jasminetesting::tests.testsuite');\n});\n";
         }
         
         $data .= "?>";
